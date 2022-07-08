c39f81bda02067a5695490169deb9eed2ed73f83
SoUsed <pro1007ka@gmail.com>
2022-07-08 12:25:08 +0300

    array.h access member functions

--- array.h
+++ array.h
@@ -1,24 +1,47 @@
 #ifndef ARRAY_H
 
+#include <iostream>
+
 template <class T, int size> class array
 {
 private:
-    T* __array;
+    T __array[size];
 public:
-    array()
-    {
-        __array = new T[size];
-    }
-
-    ~array()
-    {
-        delete [] __array;
-    }
+    array(){};
     //array(const array<T, size> &);
     //array(array<T, size> &&);
 
-    //T& operator[](int);
-    //void swap(int, int);
+
+    T& operator[](int index)
+    {
+        return __array[index];
+    }
+
+
+    T& at(int index)
+    {
+        if(index < 0 || index >= size)
+            throw "Index out of bounds!";
+        return __array[index];
+    }
+
+
+    T* data()
+    {
+        return __array;
+    }
+
+
+    T& front()
+    {
+        return &__array[0];
+    }
+
+
+    T& back()
+    {
+        return &__array[size-1];
+    }
 };
 
 #endif // !ARRAY_H
--- main.cpp
+++ main.cpp
@@ -1,7 +1,30 @@
 #include <iostream>
 #include "array.h"
+
 int main() 
 {
     array<int, 6> ar;
-    std::cout << "Hello, world!\n";
+    for(int i=0;i<6;i++)
+    {
+        ar[i] = i*i;
+    }
+
+    for(int i=0;i<6;i++)
+    {
+        std::cout << ar[i] << " ";
+    }
+    std::cout << "\n";
+
+    try
+    {
+        int x = ar.at(23);
+        int y = ar.at(-5153);
+        std::cout << x << " | " << y << "\n";
+    }
+    catch(const char* errmsg)
+    {
+        std::cerr << errmsg << "\n";
+    }
+    
+    return 0;
 }
